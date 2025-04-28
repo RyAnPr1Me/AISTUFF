4c6447ccfb07b7c041a7e24382b809772fd5f60a
BIGRMAN28 <128552151+RyAnPr1Me@users.noreply.github.com>
2025-04-28 13:24:04 +0000

    e

--- download_and_prepare_stock_data.py
+++ download_and_prepare_stock_data.py
@@ -1,5 +1,5 @@
 # --- Ticker configuration section ---
-TICKER = "INTC"  # Changed from NVDA to AAPL as a more reliable alternative
+TICKER = "IBM"  # Changed from NVDA to AAPL as a more reliable alternative
 # ------------------------------------
 
 import os
